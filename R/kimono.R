0ad19e9817cfa067da4c11c55c62fedfdc109676
MichaelLauber <michael.lauber@tum.de>
2022-03-12 13:12:15 +0100

    Added tuning of weight power in HM Lasso

--- R/kimono.R
+++ R/kimono.R
@@ -108,29 +108,37 @@ infer_network <- function(input_data, prior_network,  min_features = 2, sel_iter
           foldid2 <- sample(1:nfolds, (n %% nfolds), replace=FALSE)
           foldid <- c(foldid1, foldid2)
           
-          cv_fit <- list()
-          switch(method,
-                 lasso_coco={
-                   cv_fit <- hmlasso::cv.hmlasso(x, y, nlambda=nlambdas, lambda.min.ratio=1e-1,
-                                                 foldid=foldid, direct_prediction=TRUE,
-                                                 positify="admm_max", weight_power = 0)
-                 },
-                 lasso_hm={
-                   cv_fit <- hmlasso::cv.hmlasso(x, y, nlambda=nlambdas, lambda.min.ratio=1e-1,
-                                                 foldid=foldid, direct_prediction=TRUE,
-                                                 positify="admm_frob", weight_power = 1)
-                 }
-          )
+          weight_powers <- c(0, 0.5, 1, 1.5, 2)
           
+          if(method == "lasso_coco"){weight_powers <- c(0)}
+          
+          fits <- vector("list", length= length(weight_powers))
+          MSEs <- vector("numeric", length= length(weight_powers))
+          for(i in seq_along(weight_powers)){
+            switch(method,
+                   lasso_coco={
+                     fits[[i]] <- hmlasso::cv.hmlasso(x, y,nlambda=nlambdas, lambda.min.ratio=1e-1,
+                                                      foldid=foldid, direct_prediction=TRUE,
+                                                      positify="admm_max", weight_power = weight_powers[i])
+                   },
+                   lasso_hm={
+                     fits[[i]] <- hmlasso::cv.hmlasso(x, y, nlambda=nlambdas, lambda.min.ratio=1e-1,
+                                                      foldid=foldid, direct_prediction=TRUE,
+                                                      positify="admm_frob", weight_power = weight_powers[i])
+                   }
+            )
+            
+            beta <- fits[[i]]$fit$beta[, fits[[i]]$lambda.min.index]
+            y_hat <- x %*% beta
+            MSEs[i]<- calc_mse(y,y_hat)
+          }
+          
+          cv_fit <-  fits[[which.min(MSEs)]]
           beta <- cv_fit$fit$beta[, cv_fit$lambda.min.index]
-          
-          pred <- predict(cv_fit$fit, x)
-          
           y_hat <- x %*% beta
-          
-          r_squared <- calc_r_square(y, y_hat )
-          
           mse <- calc_mse(y,y_hat)
+          pred <- predict(cv_fit$fit, x)
+          r_squared <- calc_r_square(y, y_hat )
           
           covariates  <- rownames(cv_fit$fit$beta)
           
